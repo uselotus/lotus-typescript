276168c70bec794a7a9f1c4c3de4c94cdff95a90
mnida <mikaelnida@gmail.com>
2022-12-20 00:47:08 -0800

    pray to god

--- src/responses/CustomerFeatureAccess.ts
+++ src/responses/CustomerFeatureAccess.ts
@@ -1,29 +1,26 @@
 export interface SubscriptionFilters {
-    property1: string;
-    property2: string;
+  value: string;
+  property_name: string;
 }
 
 export interface CustomerFeatureAccessResponse {
-    feature_name: string;
-    plan_id: string;
-    subscription_filters: SubscriptionFilters;
-    access: boolean;
+  feature_name: string;
+  plan_id: string;
+  subscription_filters: SubscriptionFilters[];
+  access: boolean;
 }
 
 export interface UsagePerComponent {
-    event_name: string;
-    metric_name: string;
-    metric_id: string;
-    metric_usage: number;
-    metric_free_limit: number;
-    metric_total_limit: number;
+  event_name: string;
+  metric_name: string;
+  metric_id: string;
+  metric_usage: number;
+  metric_free_limit: number;
+  metric_total_limit: number;
 }
 
 export interface CustomerMetricAccessResponse {
-    plan_id: string;
-    subscription_filters: SubscriptionFilters[];
-    usage_per_component: UsagePerComponent[];
+  plan_id: string;
+  subscription_filters: SubscriptionFilters[];
+  usage_per_component: UsagePerComponent[];
 }
-
-
-
